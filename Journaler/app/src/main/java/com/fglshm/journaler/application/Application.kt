dae03e26821a856caf47e93041bbc9732eeaf0c1
fglshm <fglshm@gmail.com>
2019-05-27 22:07:51 +0900

    commit

---
+++ Journaler/app/src/main/java/com/fglshm/journaler/application/Application.kt
@@ -0,0 +1,17 @@
+package com.fglshm.journaler.application
+
+import android.app.Application
+import android.content.Context
+
+class Journaler : Application() {
+
+    companion object {
+        var ctx: Context? = null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        ctx = applicationContext
+    }
+
+}
